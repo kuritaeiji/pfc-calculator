b55179fbdad11025e5651b40855c5734c6e09e15
kuritaeiji <eiji199825@yahoo.co.jp>
2021-12-21 05:33:36 +0900

    カテゴリー

--- pages/foods/index.vue
+++ pages/foods/index.vue
@@ -1,13 +1,38 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <sub-header :title="title" />
+
+    <v-tabs v-model="tab" background-color="grey lighten-4" centered class="mb-4">
+      <v-tab v-for="category of categories" :key="`${category.id}-tab`">
+        {{ category.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="category of categories" :key="`${category.id}-tab-item`" transition="false">
+        {{ category.name }}
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+  data () {
+    return {
+      title: this.$t('title.foods.index'),
+      tab: null
+    }
+  },
   head () {
     return {
-      title: this.$t('title.foods.index')
+      title: this.title
     }
+  },
+  computed: {
+    ...mapGetters('category', ['categories'])
   }
 }
 </script>
---
+++ store/category.js
@@ -0,0 +1,42 @@
+export const state = () => ({
+  categories: [
+    { id: 1, name: '主食' },
+    { id: 2, name: '肉類' }
+  ],
+  currentId: 2
+})
+
+export const getters = {
+  categories (state) {
+    return state.categories
+  },
+  categoryIndexById (state) {
+    return id => state.categories.findIndex(c => c.id === id)
+  }
+}
+
+export const actions = {
+  addCategory ({ commit }, payload) {
+    commit('addCategory', payload)
+  },
+  removeCategory ({ commit, getters }, payload) {
+    const index = getters.categoryIndexById(payload.category.id)
+    commit('removeCategory', { index })
+  },
+  updateCategory ({ commit }, payload) {
+    const index = getters.categoryIndexById(payload.catetgory.id)
+    commit('updateCategory', { ...payload, index })
+  }
+}
+
+export const mutations = {
+  addCategory (state, { category }) {
+    state.categories.push({ ...category, id: ++state.currentId })
+  },
+  removeCategory (state, { index }) {
+    state.categories.splice(index, 1)
+  },
+  updateCategory (state, { category, index }) {
+    state.categories.splice(index, 1, category)
+  }
+}
