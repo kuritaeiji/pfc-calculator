99177d42b0f12b39c825e479ef2341a21cca5aae
kuritaeiji <eiji199825@yahoo.co.jp>
2021-12-26 06:02:42 +0900

    体重と体脂肪の編集のコンポーネント化

---
+++ components/BodyCardText.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card-text class="grey--text font-weight-bold">
+    <div class="text-subtitle-1">
+      {{ $t(`model.body.${dataName}`) }}
+    </div>
+    <div class="text-h5 d-flex">
+      <span v-show="!isEditing">{{ data }}kg</span>
+      <v-form v-show="isEditing" ref="form" @submit.prevent>
+        <v-text-field v-model.number="_updatingData" :rules="rules" @keyup.enter="finishEdit" />
+      </v-form>
+      <v-btn icon color="grey" class="ml-3" @click="startEdit">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </div>
+  </v-card-text>
+</template>
+
+<script>
+import { required } from '@/validators/validators'
+
+export default {
+  props: {
+    dataName: {
+      type: String,
+      required: true
+    },
+    data: {
+      type: Number,
+      required: true
+    },
+    updatingData: {
+      type: Number,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isEditing: false,
+      rules: [required]
+    }
+  },
+  computed: {
+    _updatingData: {
+      get () {
+        return this.updatingData
+      },
+      set (value) {
+        this.$emit('update:updating-data', value)
+      }
+    }
+  },
+  methods: {
+    startEdit () {
+      this.$emit('startEdit')
+      this.isEditing = true
+    },
+    finishEdit () {
+      if (this.$refs.form.validate()) {
+        this.$emit('finishEdit')
+        this.isEditing = false
+      }
+    }
+  }
+}
+</script>
--- pages/dates/_date.vue
+++ pages/dates/_date.vue
@@ -3,38 +3,27 @@
     <sub-header :title="date.dating" />
 
     <v-card flat tile max-width="500" class="d-flex">
-      <v-card-text class="grey--text font-weight-bold">
-        <div class="text-subtitle-1">
-          {{ $t('model.body.weight') }}
-        </div>
-        <div class="text-h5 d-flex">
-          <span v-show="!editingWeight">{{ body.weight }}kg</span>
-          <v-text-field v-show="editingWeight" v-model.number="updatingWeigth" :rules="bodyRules" @keyup.enter="updateWeightTemplate" />
-          <v-btn icon color="grey" class="ml-3" @click="startEditWeight">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-        </div>
-      </v-card-text>
+      <body-card-text
+        data-name="weight"
+        :data="body.weight"
+        :updating-data.sync="updatingWeigth"
+        @startEdit="startEditWeight"
+        @finishEdit="updateWeightTemplate"
+      />
 
-      <v-card-text class="grey--text font-weight-bold">
-        <div class="text-subtitle-1">
-          {{ $t('model.body.fatPercentage') }}
-        </div>
-        <div class="text-h5 d-flex">
-          <span v-show="!editingFatPercentage">{{ body.fatPercentage }}%</span>
-          <v-text-field v-show="editingFatPercentage" v-model="updatingFatPercentage" :rules="bodyRules" @keyup.enter="updateFatPercentageTemplate" />
-          <v-btn icon color="grey" class="ml-3" @click="startEditFatPercentage">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-        </div>
-      </v-card-text>
+      <body-card-text
+        data-name="fatPercentage"
+        :data="body.fatPercentage"
+        :updating-data.sync="updatingFatPercentage"
+        @startEdit="startEditFatPercentage"
+        @finishEdit="updateFatPercentageTemplate"
+      />
     </v-card>
   </v-container>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
-import { required } from '@/validators/validators'
 
 export default {
   middleware: ['validDate', 'createDate'],
@@ -45,11 +34,8 @@ export default {
   },
   data () {
     return {
-      editingWeight: false,
-      editingFatPercentage: false,
       updatingWeigth: 0,
-      updatingFatPercentage: 0,
-      bodyRules: [required]
+      updatingFatPercentage: 0
     }
   },
   head () {
@@ -61,19 +47,15 @@ export default {
     ...mapActions('body', ['updateWeight', 'updateFatPercentage']),
     startEditWeight () {
       this.updatingWeigth = this.body.weight
-      this.editingWeight = true
     },
     updateWeightTemplate () {
       this.updateWeight({ weight: this.updatingWeigth, date: this.date })
-      this.editingWeight = false
     },
     startEditFatPercentage () {
       this.updatingFatPercentage = this.body.fatPercentage
-      this.editingFatPercentage = true
     },
     updateFatPercentageTemplate () {
       this.updateFatPercentage({ fatPercentage: this.updatingFatPercentage, date: this.date })
-      this.editingFatPercentage = false
     }
   }
 }
